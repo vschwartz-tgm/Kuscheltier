adb527dd087531786bae6e75364007ade8d194b9
mwintersperger-tgm <mwintersperger@student.tgm.ac.at>
2018-05-15 08:57:02 +0200

    SimonSays begonnen

---
+++ SimonSays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+
+class simonSagt(object):
+
+    def __init__(self,highscore):
+        self.highscore = highscore
+        self.runSpiel()
+
+
+    def runSpiel(self):
+        print("Die Regeln sind einfach.")
+        print("Wenn ich zum Beispiel sage 'Simon sagt das sie meine Linke Pfote drücken sollen', dann müssen sie dies machen.")
+        print("Wenn ich das 'Simon sagt' weklasse sollen sie nichts machen")
+        while True:
+            pass
