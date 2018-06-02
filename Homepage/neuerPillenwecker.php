5498931994f09d0612764aaea9a9f262ed2d8a5a
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-06-02 16:48:10 +0200

    Herauslesen checkboxen

--- Homepage/neuerPillenwecker.php
+++ Homepage/neuerPillenwecker.php
@@ -1,6 +1,29 @@
 <?php
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])){
 		// ToDo: Werte rauslesen & in DB speichern
+		if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])){
+			if ($_POST["Montag"] == "1"){
+				// Montag angeklickt
+			}
+			if ($_POST["Dienstag"] == "1"){
+				// Dienstag angeklickt
+			}
+			if ($_POST["Mittwoch"] == "1"){
+				// Mittwoch angeklickt
+			}
+			if ($_POST["Donnerstag"] == "1"){
+				// Donnerstag angeklickt
+			}
+			if ($_POST["Freitag"] == "1"){
+				// Freitag angeklickt
+			}
+			if ($_POST["Samstag"] == "1"){
+				// Samstag angeklickt
+			}
+			if ($_POST["Sonntag"] == "1"){
+				// Sonntag angeklickt
+			}
+		}
 	}
 ?>
 <html>
@@ -29,31 +52,31 @@
 					<div class="col-form-label col-sm-2"><h3>Tage</h3></div>
 					<div class="col-sm-10">
 						<div class="form-check" style="padding-top: 20px;">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox1" />
+							<input class="form-check-input" type="checkbox" value="1" name="Montag" id="checkBox1" />
 							<label class="form-check-label" for="checkBox1"><h5>Montag</h5></label>
 						</div>
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox2" />
+							<input class="form-check-input" type="checkbox" value="1" name="Dienstag" id="checkBox2" />
 							<label class="form-check-label" for="checkBox2"><h5>Dienstag</h5></label>
 						</div>
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox3" />
+							<input class="form-check-input" type="checkbox" value="1" name="Mittwoch" id="checkBox3" />
 							<label class="form-check-label" for="checkBox3"><h5>Mittwoch</h5></label>
 						</div>
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox4" />
+							<input class="form-check-input" type="checkbox" value="1" name="Donnerstag" id="checkBox4" />
 							<label class="form-check-label" for="checkBox4"><h5>Donnerstag</h5></label>
 						</div>
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox5"/>
+							<input class="form-check-input" type="checkbox" value="1" name="Freitag" id="checkBox5"/>
 							<label class="form-check-label" for="checkBox5"><h5>Freitag</h5></label>
 						</div>
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox6"/>
+							<input class="form-check-input" type="checkbox" value="1" name="Samstag" id="checkBox6"/>
 							<label class="form-check-label" for="checkBox6"><h5>Samstag</h5></label>
 						</div>
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="" id="checkBox7"/>
+							<input class="form-check-input" type="checkbox" value="" name="Sonntag" id="checkBox7"/>
 							<label class="form-check-label" for="checkBox7"><h5>Sonntag</h5></label>
 						</div>
 					</div>
