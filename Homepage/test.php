871ff94a413ccef22dd46f6ebd30b769c2c94a27
ckern-tgm <chris10.kern@gmail.com>
2018-06-02 19:41:11 +0200

    dbconn richtig

--- Homepage/test.php
+++ Homepage/test.php
@@ -1,12 +1,6 @@
 <?php
-$dbconn = pg_connect("host=localhost port=5432 dbname=teddy user=admin password=password");
-$fehler = false;
-
-if($fehler == false){
-
-	$insert = "INSERT INTO pillen VALUES('test','true', 'true','false','true','false','false','false','20:00:00');";
-	$sql = pg_query($dbconn, $insert);
-
-}
+$dbconn = pg_connect("host=localhost port=5432 dbname=teddy user=test password=admin");
+$insert = "INSERT INTO pillen VALUES('test','true', 'true','false','true','false','false','false','20:00:00');";
+$sql = pg_query($dbconn, $insert);
 
 ?>
