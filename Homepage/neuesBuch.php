b5b38657acebab53bf261f539f052f28b9f40167
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-08-23 10:13:47 +0200

    navbar fix

--- Homepage/neuesBuch.php
+++ Homepage/neuesBuch.php
@@ -23,30 +23,31 @@
 		<link href="myCSS.css" rel="stylesheet">
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-sm navbar-light border border-primary rounded" style="background-color: #66CCFF;">
-			<!-- Brand -->
-			<!--<a class="navbar-brand" href="index.html"><img src="Assets/home.png" class="nav-img"></a>-->
-			<!-- Links -->
-			<div class="collapse navbar-collapse" id="nav-content">   
-				<ul class="nav navbar-nav">
-					<li class="nav-item px-sm-2 pt-sm-3">
-						<a class="nav-link" href="index.php" style="margin-left: 36px;"><h2>Startseite</h2></a>
-					</li>
-					<li class="nav-item px-sm-2 pt-sm-3">
-						<a class="nav-link" href="pillenwecker.php"><h1>Pillenwecker</h1></a>
-					</li>
-					<li class="nav-item px-sm-2 pt-sm-3">
-						<a class="nav-link" href="terminplanung.php"><h2>Terminplanung</h2></a>
-					</li>
-					<li class="nav-item active px-sm-2 pt-sm-3">
-						<a class="nav-link" href="buechervorlesen.php"><h2>Bücher vorlesen</h2></a>
-					</li>
-					<li class="nav-item pt-sm-3">
-						<a class="nav-link" href="notfallsignal.php"><h2>Notfallsignal<h2></a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+    <nav class="navbar navbar-expand-sm navbar-light border border-primary rounded" style="background-color: #66CCFF;">
+        <!-- Brand -->
+        <!--<a class="navbar-brand" href="index.html"><img src="Assets/home.png" class="nav-img"></a>-->
+
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="nav-content">
+            <ul class="nav navbar-nav">
+                <li class="nav-item px-sm-2 pt-sm-3">
+                    <a class="nav-link" href="index.php" style="margin-left: 36px;"><h2>Startseite</h2></a>
+                </li>
+                <li class="nav-item px-sm-2 pt-sm-3">
+                    <a class="nav-link" href="pillenwecker.php"><h2>Pillenwecker</h2></a>
+                </li>
+                <li class="nav-item px-sm-2 pt-sm-3">
+                    <a class="nav-link" href="terminplanung.php"><h2>Terminplanung</h2></a>
+                </li>
+                <li class="nav-item active px-sm-2 pt-sm-3">
+                    <a class="nav-link" href="buechervorlesen.php"><h1>Bücher vorlesen</h1></a>
+                </li>
+                <li class="nav-item pt-sm-3">
+                    <a class="nav-link" href="notfallsignal.php"><h2>Notfallsignal<h2></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 		<div align="center">
 			<button type="button" class="btn btn-outline-dark btn-lg" style="float: left;" onclick ="window.location = 'buechervorlesen.php'">Zurück</button>
 			<p><h1>Vorhandene Bücher</h1></p>
